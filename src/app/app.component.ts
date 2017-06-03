67920ef324ad125c9f24edfd86ca5c841afa8b03
theunreal <theunrealz@gmail.com>
2017-06-03 22:36:56 +0300

    Switched to angular-vrviewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,ViewChild } from '@angular/core';
-declare var VRView;
+import { Scene } from './interfaces/scene.interface';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +8,9 @@ declare var VRView;
 })
 export class AppComponent {
 
-  scenes = {
+  scenes: Scene = {
     world: {
       image: 'assets/1.jpg',
-      preview: 'assets/1.jpg',
       hotspots: {
         green_area: {
           pitch: 10,
@@ -22,7 +22,6 @@ export class AppComponent {
     },
     green_area: {
       image: 'assets/2.jpg',
-      preview: 'assets/2.jpg',
       hotspots: {
         world: {
           pitch: 20,
@@ -33,47 +32,7 @@ export class AppComponent {
       }
     }
 };
-  viewer: any;
 
   constructor() {
-
-  }
-
-  ngAfterViewInit() {
-    this.viewer = new VRView.Player('#vrview', {
-      image: 'assets/blank.png',
-      width: '100%',
-      height: 400
-    });
-
-    this.viewer.on('ready',() => {
-      this.loadScene('world');
-
-      this.viewer.on('click', (event) => this.loadScene(event.id));
-    });
-  }
-
-  loadScene(id) {
-    if (id) {
-    // Set the image
-    this.viewer.setContent({
-      image: this.scenes[id].image,
-      preview: this.scenes[id].preview,
-    });
-    // Add all the hotspots for the scene
-    let newScene = this.scenes[id];
-    let sceneHotspots = Object.keys(newScene.hotspots);
-    for (let i = 0; i < sceneHotspots.length; i++) {
-      let hotspotKey = sceneHotspots[i];
-      let hotspot = newScene.hotspots[hotspotKey];
-
-      this.viewer.addHotspot(hotspotKey, {
-        pitch: hotspot.pitch,
-        yaw: hotspot.yaw,
-        radius: hotspot.radius,
-        distance: hotspot.distance
-        });
-      }
-    }
   }
 }
